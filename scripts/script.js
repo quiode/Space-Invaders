e58eea1d5dda91200e18795b985b2d3e01e4cbac
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2021-08-24 23:32:44 +0200

    v0.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,81 @@
+"use strict";
+var Player = /** @class */ (function () {
+    function Player(position, canvas) {
+        if (position === void 0) { position = { x: 0, y: 0 }; }
+        this.position = position;
+        this.canvas = canvas;
+        this.img_url = 'assets/player.png';
+    }
+    Player.prototype.draw = function () { };
+    return Player;
+}());
+var Enemy = /** @class */ (function () {
+    function Enemy(position, canvas) {
+        if (position === void 0) { position = { x: 0, y: 0 }; }
+        this.position = position;
+        this.canvas = canvas;
+        this.img_url = 'assets/player.png';
+    }
+    Enemy.prototype.move = function (x, y) {
+        if (x === void 0) { x = 0; }
+        if (y === void 0) { y = 0; }
+        this.position.x += x;
+        this.position.y += y;
+    };
+    Enemy.prototype.draw = function () { };
+    return Enemy;
+}());
+var Shot = /** @class */ (function () {
+    function Shot(position, canvas) {
+        if (position === void 0) { position = { x: 0, y: 0 }; }
+        this.position = position;
+        this.canvas = canvas;
+    }
+    Shot.prototype.draw = function () { };
+    return Shot;
+}());
+var Game = /** @class */ (function () {
+    function Game(canvas) {
+        this.canvas = canvas;
+        this.context = this.canvas.getContext('2d');
+        this.background_img = new Image();
+        this.background_img.src = 'assets/background.png';
+    }
+    Game.prototype.load_game = function () {
+        this.create_background();
+    };
+    Game.prototype.start_game = function () { };
+    Game.prototype.create_player = function () { };
+    Game.prototype.create_enemies = function () { };
+    Game.prototype.create_background = function () {
+        var _a;
+        (_a = this.context) === null || _a === void 0 ? void 0 : _a.drawImage(this.background_img, 0, 0);
+    };
+    Game.prototype.clear = function () {
+        var _a;
+        (_a = this.context) === null || _a === void 0 ? void 0 : _a.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    };
+    return Game;
+}());
 // Appears Title Screen
 window.addEventListener('load', function () {
     var title_header = document.getElementsByTagName('header')[0];
     var transition_time = 5;
     title_header.style.opacity = '1';
     title_header.style.transitionDuration = transition_time + 's';
+    // Let's the player start the game after the title screen has loaded
+    setTimeout(function () {
+        document.getElementsByTagName('canvas')[0].addEventListener('click', load_game);
+    }, transition_time * 1000);
 });
+/**
+ * @description Starts the Game
+ */
+function load_game() {
+    // Removes the title screen
+    var title_header = document.getElementsByTagName('header')[0];
+    title_header.style.display = 'none';
+    // Loads the game
+    var game = new Game(document.getElementsByTagName('canvas')[0]);
+    game.load_game();
+}
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,7 +1,84 @@
+type position = {
+  x: number;
+  y: number;
+};
+
+class Player {
+  constructor(private position: position = { x: 0, y: 0 }, private canvas: HTMLCanvasElement) {}
+  img_url = 'assets/player.png';
+
+  draw() {}
+}
+
+class Enemy {
+  constructor(private position: position = { x: 0, y: 0 }, private canvas: HTMLCanvasElement) {}
+  img_url = 'assets/player.png';
+
+  move(x: number = 0, y: number = 0) {
+    this.position.x += x;
+    this.position.y += y;
+  }
+
+  draw() {}
+}
+
+class Shot {
+  constructor(private position: position = { x: 0, y: 0 }, private canvas: HTMLCanvasElement) {}
+
+  draw() {}
+}
+
+class Game {
+  private context = this.canvas.getContext('2d');
+  private background_img = new Image();
+  constructor(private canvas: HTMLCanvasElement) {
+    this.background_img.src = 'assets/background.png';
+  }
+
+  load_game() {
+    this.create_background();
+  }
+
+  start_game() {}
+
+  create_player() {}
+
+  create_enemies() {}
+
+  create_background() {
+    this.context?.drawImage(this.background_img, 0, 0);
+  }
+
+  clear() {
+    this.context?.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
+
 // Appears Title Screen
 window.addEventListener('load', () => {
   let title_header = document.getElementsByTagName('header')[0] as unknown as HTMLHeadingElement;
   const transition_time = 5;
   title_header.style.opacity = '1';
   title_header.style.transitionDuration = transition_time + 's';
+
+  // Let's the player start the game after the title screen has loaded
+  setTimeout(() => {
+    (document.getElementsByTagName('canvas')[0] as HTMLCanvasElement).addEventListener(
+      'click',
+      load_game
+    );
+  }, transition_time * 1000);
 });
+
+/**
+ * @description Starts the Game
+ */
+function load_game() {
+  // Removes the title screen
+  let title_header = document.getElementsByTagName('header')[0] as unknown as HTMLHeadingElement;
+  title_header.style.display = 'none';
+
+  // Loads the game
+  let game = new Game(document.getElementsByTagName('canvas')[0] as HTMLCanvasElement);
+  game.load_game();
+}
