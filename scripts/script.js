62d4716d204694dfe18c3a28d2aa77bd144742f6
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2021-08-25 12:06:33 +0200

    v0.2

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,83 @@
 "use strict";
 var Player = /** @class */ (function () {
-    function Player(position, canvas) {
+    function Player(canvas, position, size) {
         if (position === void 0) { position = { x: 0, y: 0 }; }
-        this.position = position;
+        if (size === void 0) { size = 50; }
         this.canvas = canvas;
+        this.position = position;
+        this.size = size;
         this.img_url = 'assets/player.png';
+        this.shots = [];
+        this.ctx = canvas.getContext('2d');
+        this.img = new Image();
+        this.img.src = this.img_url;
     }
-    Player.prototype.draw = function () { };
+    Player.prototype.draw = function () {
+        // Draws the player
+        this.ctx.drawImage(this.img, this.position.x, this.position.y, this.size, this.size);
+        // Makes a shot
+        this.shoot();
+    };
+    Player.prototype.shoot = function () {
+        // Makes a shot
+        this.shots.push(new Shot(this.canvas, this.position));
+    };
     return Player;
 }());
 var Enemy = /** @class */ (function () {
-    function Enemy(position, canvas) {
+    function Enemy(canvas, size, position) {
         if (position === void 0) { position = { x: 0, y: 0 }; }
-        this.position = position;
         this.canvas = canvas;
-        this.img_url = 'assets/player.png';
+        this.size = size;
+        this.position = position;
+        this.img_url = 'assets/enemy.png';
+        this.go_left = false;
+        this.ctx = this.canvas.getContext('2d');
+        this.img = new Image();
+        this.img.src = this.img_url;
     }
-    Enemy.prototype.move = function (x, y) {
-        if (x === void 0) { x = 0; }
-        if (y === void 0) { y = 0; }
-        this.position.x += x;
-        this.position.y += y;
+    Enemy.prototype.move = function () {
+        var step = 0.25;
+        // Checks if the enemy as reached the left or right edge of the screen
+        if (this.position.x > this.canvas.width - this.canvas.width / 20 - this.size) {
+            this.go_left = true;
+            this.position.x -= step;
+            this.position.y += this.size;
+        }
+        else if (this.position.x < this.canvas.width / 20) {
+            this.go_left = false;
+            this.position.x += step;
+            this.position.y += this.size;
+        }
+        // Moves the enemy
+        this.position.x += this.go_left ? -step : step;
+        this.draw();
     };
-    Enemy.prototype.draw = function () { };
+    Enemy.prototype.draw = function () {
+        // Draws the enemy
+        this.ctx.drawImage(this.img, this.position.x, this.position.y, this.size, this.size);
+    };
     return Enemy;
 }());
 var Shot = /** @class */ (function () {
-    function Shot(position, canvas) {
+    function Shot(canvas, position) {
         if (position === void 0) { position = { x: 0, y: 0 }; }
-        this.position = position;
         this.canvas = canvas;
+        this.position = position;
+        this.ctx = this.canvas.getContext('2d');
     }
-    Shot.prototype.draw = function () { };
+    Shot.prototype.draw = function () {
+        // Draws the shot
+        this.ctx.moveTo(this.position.x, this.position.y);
+        this.ctx.lineWidth = 5;
+        // stroke style red in hex
+        this.ctx.strokeStyle = '#ff0000';
+        this.ctx.lineTo(this.position.x, this.position.y + 10);
+    };
+    Shot.prototype.move = function () {
+        this.position.y += 1;
+        this.draw();
+    };
     return Shot;
 }());
 var Game = /** @class */ (function () {
@@ -39,13 +85,43 @@ var Game = /** @class */ (function () {
         this.canvas = canvas;
         this.context = this.canvas.getContext('2d');
         this.background_img = new Image();
+        this.enemies = [];
     }
     Game.prototype.load_game = function () {
         this.create_background();
+        this.create_player();
+        this.create_enemies();
     };
-    Game.prototype.start_game = function () { };
-    Game.prototype.create_player = function () { };
-    Game.prototype.create_enemies = function () { };
+    Game.prototype.start_game = function () {
+        var _this = this;
+        this.load_game();
+        // Game loup
+        var loop = setInterval(function () {
+            _this.clear();
+            _this.draw_background();
+            _this.enemies.forEach(function (enemy) {
+                enemy.move();
+            });
+            _this.player.draw();
+        }, 1000 / 60);
+    };
+    Game.prototype.create_player = function () {
+        var size = 25;
+        this.player = new Player(this.canvas, {
+            x: this.canvas.width / 2 - size / 2,
+            y: this.canvas.height - size - size / 5,
+        }, size);
+    };
+    Game.prototype.create_enemies = function () {
+        var row_amount = 10;
+        var size = (this.canvas.width - this.canvas.width / 10) / 2;
+        for (var i = 0; i < row_amount; i++) {
+            this.enemies.push(new Enemy(this.canvas, size / row_amount, {
+                x: (size / row_amount) * i + this.canvas.width / 20,
+                y: 0,
+            }));
+        }
+    };
     Game.prototype.create_background = function () {
         var _this = this;
         this.background_img.onload = function () {
@@ -53,6 +129,10 @@ var Game = /** @class */ (function () {
             (_a = _this.context) === null || _a === void 0 ? void 0 : _a.drawImage(_this.background_img, 0, 0, _this.canvas.width, _this.canvas.height);
         };
         this.background_img.src = 'assets/background.jpg';
+    };
+    Game.prototype.draw_background = function () {
+        var _a;
+        (_a = this.context) === null || _a === void 0 ? void 0 : _a.drawImage(this.background_img, 0, 0, this.canvas.width, this.canvas.height);
     };
     Game.prototype.clear = function () {
         var _a;
@@ -76,6 +156,6 @@ function load_game() {
     title_header.style.display = 'none';
     // Loads the game
     var game = new Game(document.getElementsByTagName('canvas')[0]);
-    game.load_game();
+    game.start_game();
 }
 //# sourceMappingURL=script.js.map
--- scripts/script.ts
+++ scripts/script.ts
@@ -4,53 +4,158 @@ type position = {
 };
 
 class Player {
-  constructor(private position: position = { x: 0, y: 0 }, private canvas: HTMLCanvasElement) {}
-  img_url = 'assets/player.png';
+  constructor(
+    private canvas: HTMLCanvasElement,
+    private position: position = { x: 0, y: 0 },
+    private size = 50
+  ) {
+    this.ctx = canvas.getContext('2d')!;
+    this.img = new Image();
+    this.img.src = this.img_url;
+  }
+  private img_url = 'assets/player.png';
+  private ctx: CanvasRenderingContext2D;
+  private img: HTMLImageElement;
+  private shots: Shot[] = [];
 
-  draw() {}
+  draw() {
+    // Draws the player
+    this.ctx.drawImage(this.img, this.position.x, this.position.y, this.size, this.size);
+
+    // Makes a shot
+    this.shoot();
+  }
+
+  private shoot() {
+    // Makes a shot
+    this.shots.push(new Shot(this.canvas, this.position));
+  }
 }
 
 class Enemy {
-  constructor(private position: position = { x: 0, y: 0 }, private canvas: HTMLCanvasElement) {}
-  img_url = 'assets/player.png';
+  constructor(
+    private canvas: HTMLCanvasElement,
+    private size: number,
+    private position: position = { x: 0, y: 0 }
+  ) {
+    this.ctx = this.canvas.getContext('2d')!;
+    this.img = new Image();
+    this.img.src = this.img_url;
+  }
+  private img_url = 'assets/enemy.png';
+  private img: HTMLImageElement;
+  private go_left = false;
+  private ctx: CanvasRenderingContext2D;
 
-  move(x: number = 0, y: number = 0) {
-    this.position.x += x;
-    this.position.y += y;
+  move() {
+    let step = 0.25;
+    // Checks if the enemy as reached the left or right edge of the screen
+    if (this.position.x > this.canvas.width - this.canvas.width / 20 - this.size) {
+      this.go_left = true;
+      this.position.x -= step;
+      this.position.y += this.size;
+    } else if (this.position.x < this.canvas.width / 20) {
+      this.go_left = false;
+      this.position.x += step;
+      this.position.y += this.size;
+    }
+
+    // Moves the enemy
+    this.position.x += this.go_left ? -step : step;
+    this.draw();
   }
 
-  draw() {}
+  private draw() {
+    // Draws the enemy
+    this.ctx.drawImage(this.img, this.position.x, this.position.y, this.size, this.size);
+  }
 }
 
 class Shot {
-  constructor(private position: position = { x: 0, y: 0 }, private canvas: HTMLCanvasElement) {}
+  ctx: CanvasRenderingContext2D;
+  constructor(private canvas: HTMLCanvasElement, private position: position = { x: 0, y: 0 }) {
+    this.ctx = this.canvas.getContext('2d')!;
+  }
 
-  draw() {}
+  private draw() {
+    // Draws the shot
+    this.ctx.moveTo(this.position.x, this.position.y);
+    this.ctx.lineWidth = 5;
+    // stroke style red in hex
+    this.ctx.strokeStyle = '#ff0000';
+    this.ctx.lineTo(this.position.x, this.position.y + 10);
+  }
+
+  move() {
+    this.position.y += 1;
+    this.draw();
+  }
 }
 
 class Game {
   private context = this.canvas.getContext('2d');
   private background_img = new Image();
   constructor(private canvas: HTMLCanvasElement) {}
+  player!: Player;
+  enemies: Enemy[] = [];
 
-  load_game() {
+  private load_game() {
     this.create_background();
+    this.create_player();
+    this.create_enemies();
   }
 
-  start_game() {}
+  public start_game() {
+    this.load_game();
+    // Game loup
+    let loop = setInterval(() => {
+      this.clear();
 
-  create_player() {}
+      this.draw_background();
+      this.enemies.forEach((enemy) => {
+        enemy.move();
+      });
+      this.player.draw();
+    }, 1000 / 60);
+  }
 
-  create_enemies() {}
+  private create_player() {
+    let size = 25;
+    this.player = new Player(
+      this.canvas,
+      {
+        x: this.canvas.width / 2 - size / 2,
+        y: this.canvas.height - size - size / 5,
+      },
+      size
+    );
+  }
 
-  create_background() {
+  private create_enemies() {
+    let row_amount = 10;
+    let size = (this.canvas.width - this.canvas.width / 10) / 2;
+    for (let i = 0; i < row_amount; i++) {
+      this.enemies.push(
+        new Enemy(this.canvas, size / row_amount, {
+          x: (size / row_amount) * i + this.canvas.width / 20,
+          y: 0,
+        })
+      );
+    }
+  }
+
+  private create_background() {
     this.background_img.onload = () => {
       this.context?.drawImage(this.background_img, 0, 0, this.canvas.width, this.canvas.height);
     };
     this.background_img.src = 'assets/background.jpg';
   }
 
-  clear() {
+  private draw_background() {
+    this.context?.drawImage(this.background_img, 0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  private clear() {
     this.context?.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
@@ -77,5 +182,5 @@ function load_game() {
 
   // Loads the game
   let game = new Game(document.getElementsByTagName('canvas')[0] as HTMLCanvasElement);
-  game.load_game();
+  game.start_game();
 }
