5f8610c1b1aa0a2033c7e391657f99ba52a47b6b
Noobmaster23 <51075975+quiode@users.noreply.github.com>
2021-08-25 18:25:51 +0200

    v0.9

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,78 @@
 "use strict";
+// Game Event
+var win_event = new CustomEvent('game_won');
+var loose_event = new CustomEvent('game_lost');
 var Player = /** @class */ (function () {
     function Player(canvas, position, size) {
+        var _this = this;
         if (position === void 0) { position = { x: 0, y: 0 }; }
         if (size === void 0) { size = 50; }
         this.canvas = canvas;
         this.position = position;
         this.size = size;
+        this.shouldMove = false; // if player moves
+        /**
+         * true = right
+         */
+        this.direction = false; // direction = true -> right
         this.img_url = 'assets/player.png';
         this.shots = [];
+        this.shootingCounter = 0;
         this.ctx = canvas.getContext('2d');
         this.img = new Image();
         this.img.src = this.img_url;
+        // For controls
+        document.addEventListener('keydown', function (e) {
+            if (e.code === 'ArrowLeft' || e.code === 'KeyA') {
+                _this.shouldMove = true;
+                _this.direction = false;
+            }
+            else if (e.code === 'ArrowRight' || e.code === 'KeyD') {
+                _this.shouldMove = true;
+                _this.direction = true;
+            }
+        });
+        document.addEventListener('keyup', function (e) {
+            if (e.code === 'ArrowLeft' ||
+                e.code === 'KeyA' ||
+                e.code === 'ArrowRight' ||
+                e.code === 'KeyD') {
+                _this.shouldMove = false;
+            }
+        });
     }
     Player.prototype.draw = function () {
+        // Moves the player
+        this.move();
         // Draws the player
         this.ctx.drawImage(this.img, this.position.x, this.position.y, this.size, this.size);
         // Makes a shot
-        this.shoot();
+        if (this.shootingCounter >= Player.shootingSpead) {
+            this.shoot();
+            this.shootingCounter = 0;
+        }
+        this.shootingCounter++;
     };
     Player.prototype.shoot = function () {
         // Makes a shot
-        this.shots.push(new Shot(this.canvas, this.position));
+        this.shots.push(new Shot(this.canvas, {
+            x: this.position.x + this.size / 2,
+            y: this.position.y - this.size / 2,
+        }));
     };
+    Player.prototype.move = function () {
+        var step = 1;
+        var buffer = this.size / 5;
+        if (this.shouldMove) {
+            if (this.direction && this.position.x + this.size < this.canvas.width - buffer) {
+                this.position.x += step;
+            }
+            else if (!this.direction && this.position.x > 0 + buffer) {
+                this.position.x -= step;
+            }
+        }
+    };
+    Player.shootingSpead = 60 * 1; // in frames
     return Player;
 }());
 var Enemy = /** @class */ (function () {
@@ -32,6 +83,7 @@ var Enemy = /** @class */ (function () {
         this.position = position;
         this.img_url = 'assets/enemy.png';
         this.go_left = false;
+        this.health = 100;
         this.ctx = this.canvas.getContext('2d');
         this.img = new Image();
         this.img.src = this.img_url;
@@ -51,6 +103,9 @@ var Enemy = /** @class */ (function () {
         }
         // Moves the enemy
         this.position.x += this.go_left ? -step : step;
+        if (this.position.y > (this.canvas.height / 10) * 8) {
+            document.dispatchEvent(loose_event);
+        }
         this.draw();
     };
     Enemy.prototype.draw = function () {
@@ -64,28 +119,60 @@ var Shot = /** @class */ (function () {
         if (position === void 0) { position = { x: 0, y: 0 }; }
         this.canvas = canvas;
         this.position = position;
+        this.strokeLength = 10;
         this.ctx = this.canvas.getContext('2d');
     }
     Shot.prototype.draw = function () {
         // Draws the shot
+        this.ctx.beginPath();
         this.ctx.moveTo(this.position.x, this.position.y);
-        this.ctx.lineWidth = 5;
+        this.ctx.lineWidth = 2;
         // stroke style red in hex
         this.ctx.strokeStyle = '#ff0000';
-        this.ctx.lineTo(this.position.x, this.position.y + 10);
+        this.ctx.lineTo(this.position.x, this.position.y + this.strokeLength);
+        this.ctx.stroke();
     };
     Shot.prototype.move = function () {
-        this.position.y += 1;
+        this.position.y -= 1;
         this.draw();
+    };
+    Shot.prototype.collisionCheck = function (enemies, wave, maxWaves) {
+        var _this = this;
+        // Checks if the shot has collided with an enemy
+        var collided = false;
+        enemies.forEach(function (enemy) {
+            if (_this.position.x >= enemy.position.x &&
+                _this.position.x <= enemy.position.x + enemy.size &&
+                _this.position.y >= enemy.position.y &&
+                _this.position.y <= enemy.position.y + enemy.size) {
+                collided = true;
+                enemy.health -= 10 * (maxWaves / wave);
+                if (enemy.health <= 0) {
+                    enemies.splice(enemies.indexOf(enemy), 1);
+                    if (wave == maxWaves && enemies.length === 0) {
+                        document.dispatchEvent(win_event);
+                    }
+                }
+            }
+        });
+        return collided;
     };
     return Shot;
 }());
 var Game = /** @class */ (function () {
     function Game(canvas) {
+        var _this = this;
         this.canvas = canvas;
         this.context = this.canvas.getContext('2d');
         this.background_img = new Image();
         this.enemies = [];
+        // Subscribe to events
+        document.addEventListener('game_won', function () {
+            _this.win_screen();
+        });
+        document.addEventListener('game_lost', function () {
+            _this.loose_screen();
+        });
     }
     Game.prototype.load_game = function () {
         this.create_background();
@@ -94,15 +181,36 @@ var Game = /** @class */ (function () {
     };
     Game.prototype.start_game = function () {
         var _this = this;
+        var waves = 10;
+        var wave = 1;
+        var spawn_rate = 60 * 15; // in framess
+        var spawn_rate_counter = 0;
         this.load_game();
         // Game loup
         var loop = setInterval(function () {
             _this.clear();
+            // Spawns enemies
+            if (spawn_rate_counter >= spawn_rate) {
+                if (wave <= waves) {
+                    _this.create_enemies();
+                    wave++;
+                    spawn_rate_counter = 0;
+                }
+            }
+            spawn_rate_counter++;
             _this.draw_background();
             _this.enemies.forEach(function (enemy) {
                 enemy.move();
             });
             _this.player.draw();
+            _this.player.shots.forEach(function (shot) {
+                if (shot.collisionCheck(_this.enemies, wave, waves)) {
+                    _this.player.shots.splice(_this.player.shots.indexOf(shot), 1);
+                }
+                else {
+                    shot.move();
+                }
+            });
         }, 1000 / 60);
     };
     Game.prototype.create_player = function () {
@@ -137,6 +245,16 @@ var Game = /** @class */ (function () {
     Game.prototype.clear = function () {
         var _a;
         (_a = this.context) === null || _a === void 0 ? void 0 : _a.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    };
+    Game.prototype.loose_screen = function () {
+        alert('You loose');
+        // Reload the page
+        location.reload();
+    };
+    Game.prototype.win_screen = function () {
+        alert('You win');
+        // Reload the page
+        location.reload();
     };
     return Game;
 }());
--- scripts/script.ts
+++ scripts/script.ts
@@ -3,6 +3,10 @@ type position = {
   y: number;
 };
 
+// Game Event
+const win_event = new CustomEvent('game_won');
+const loose_event = new CustomEvent('game_lost');
+
 class Player {
   constructor(
     private canvas: HTMLCanvasElement,
@@ -12,31 +16,82 @@ class Player {
     this.ctx = canvas.getContext('2d')!;
     this.img = new Image();
     this.img.src = this.img_url;
+    // For controls
+    document.addEventListener('keydown', (e: KeyboardEvent) => {
+      if (e.code === 'ArrowLeft' || e.code === 'KeyA') {
+        this.shouldMove = true;
+        this.direction = false;
+      } else if (e.code === 'ArrowRight' || e.code === 'KeyD') {
+        this.shouldMove = true;
+        this.direction = true;
+      }
+    });
+    document.addEventListener('keyup', (e: KeyboardEvent) => {
+      if (
+        e.code === 'ArrowLeft' ||
+        e.code === 'KeyA' ||
+        e.code === 'ArrowRight' ||
+        e.code === 'KeyD'
+      ) {
+        this.shouldMove = false;
+      }
+    });
   }
+  private shouldMove: boolean = false; // if player moves
+  /**
+   * true = right
+   */
+  private direction: boolean = false; // direction = true -> right
   private img_url = 'assets/player.png';
   private ctx: CanvasRenderingContext2D;
   private img: HTMLImageElement;
-  private shots: Shot[] = [];
+  public shots: Shot[] = [];
+  private static shootingSpead = 60 * 1; // in frames
+  private shootingCounter = 0;
 
   draw() {
+    // Moves the player
+    this.move();
+
     // Draws the player
     this.ctx.drawImage(this.img, this.position.x, this.position.y, this.size, this.size);
 
     // Makes a shot
-    this.shoot();
+    if (this.shootingCounter >= Player.shootingSpead) {
+      this.shoot();
+      this.shootingCounter = 0;
+    }
+    this.shootingCounter++;
   }
 
   private shoot() {
     // Makes a shot
-    this.shots.push(new Shot(this.canvas, this.position));
+    this.shots.push(
+      new Shot(this.canvas, {
+        x: this.position.x + this.size / 2,
+        y: this.position.y - this.size / 2,
+      })
+    );
+  }
+
+  private move() {
+    const step = 1;
+    const buffer = this.size / 5;
+    if (this.shouldMove) {
+      if (this.direction && this.position.x + this.size < this.canvas.width - buffer) {
+        this.position.x += step;
+      } else if (!this.direction && this.position.x > 0 + buffer) {
+        this.position.x -= step;
+      }
+    }
   }
 }
 
 class Enemy {
   constructor(
     private canvas: HTMLCanvasElement,
-    private size: number,
-    private position: position = { x: 0, y: 0 }
+    public size: number,
+    public position: position = { x: 0, y: 0 }
   ) {
     this.ctx = this.canvas.getContext('2d')!;
     this.img = new Image();
@@ -46,6 +101,7 @@ class Enemy {
   private img: HTMLImageElement;
   private go_left = false;
   private ctx: CanvasRenderingContext2D;
+  public health = 100;
 
   move() {
     let step = 0.25;
@@ -62,6 +118,9 @@ class Enemy {
 
     // Moves the enemy
     this.position.x += this.go_left ? -step : step;
+    if (this.position.y > (this.canvas.height / 10) * 8) {
+      document.dispatchEvent(loose_event);
+    }
     this.draw();
   }
 
@@ -72,30 +131,64 @@ class Enemy {
 }
 
 class Shot {
-  ctx: CanvasRenderingContext2D;
+  private ctx: CanvasRenderingContext2D;
   constructor(private canvas: HTMLCanvasElement, private position: position = { x: 0, y: 0 }) {
     this.ctx = this.canvas.getContext('2d')!;
   }
+  private strokeLength = 10;
 
   private draw() {
     // Draws the shot
+    this.ctx.beginPath();
     this.ctx.moveTo(this.position.x, this.position.y);
-    this.ctx.lineWidth = 5;
+    this.ctx.lineWidth = 2;
     // stroke style red in hex
     this.ctx.strokeStyle = '#ff0000';
-    this.ctx.lineTo(this.position.x, this.position.y + 10);
+    this.ctx.lineTo(this.position.x, this.position.y + this.strokeLength);
+    this.ctx.stroke();
   }
 
   move() {
-    this.position.y += 1;
+    this.position.y -= 1;
     this.draw();
+  }
+
+  collisionCheck(enemies: Enemy[], wave: number, maxWaves: number) {
+    // Checks if the shot has collided with an enemy
+    let collided = false;
+    enemies.forEach((enemy) => {
+      if (
+        this.position.x >= enemy.position.x &&
+        this.position.x <= enemy.position.x + enemy.size &&
+        this.position.y >= enemy.position.y &&
+        this.position.y <= enemy.position.y + enemy.size
+      ) {
+        collided = true;
+        enemy.health -= 10 * (maxWaves / wave);
+        if (enemy.health <= 0) {
+          enemies.splice(enemies.indexOf(enemy), 1);
+          if (wave == maxWaves && enemies.length === 0) {
+            document.dispatchEvent(win_event);
+          }
+        }
+      }
+    });
+    return collided;
   }
 }
 
 class Game {
   private context = this.canvas.getContext('2d');
   private background_img = new Image();
-  constructor(private canvas: HTMLCanvasElement) {}
+  constructor(private canvas: HTMLCanvasElement) {
+    // Subscribe to events
+    document.addEventListener('game_won', () => {
+      this.win_screen();
+    });
+    document.addEventListener('game_lost', () => {
+      this.loose_screen();
+    });
+  }
   player!: Player;
   enemies: Enemy[] = [];
 
@@ -106,16 +199,38 @@ class Game {
   }
 
   public start_game() {
+    const waves = 10;
+    let wave = 1;
+    const spawn_rate = 60 * 15; // in framess
+    let spawn_rate_counter = 0;
+
     this.load_game();
     // Game loup
     let loop = setInterval(() => {
       this.clear();
+
+      // Spawns enemies
+      if (spawn_rate_counter >= spawn_rate) {
+        if (wave <= waves) {
+          this.create_enemies();
+          wave++;
+          spawn_rate_counter = 0;
+        }
+      }
+      spawn_rate_counter++;
 
       this.draw_background();
       this.enemies.forEach((enemy) => {
         enemy.move();
       });
       this.player.draw();
+      this.player.shots.forEach((shot) => {
+        if (shot.collisionCheck(this.enemies, wave, waves)) {
+          this.player.shots.splice(this.player.shots.indexOf(shot), 1);
+        } else {
+          shot.move();
+        }
+      });
     }, 1000 / 60);
   }
 
@@ -157,6 +272,18 @@ class Game {
 
   private clear() {
     this.context?.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  private loose_screen() {
+    alert('You loose');
+    // Reload the page
+    location.reload();
+  }
+
+  private win_screen() {
+    alert('You win');
+    // Reload the page
+    location.reload();
   }
 }
 
